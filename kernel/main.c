62cc8bd3cf63e56866f3a23edcb4d7151b823b31
ramtinmk <ramtinmk1382@gmail.com>
2025-04-18 21:47:52 +0330

    fixed some comments

--- kernel/main.c
+++ kernel/main.c
@@ -20,11 +20,10 @@ main()
     kinit();         // physical page allocator
 
 
-    // <-- 3. Add your custom log messages using the macros -->
+    // Add your custom log messages using the macros -->
     log_info("Welcome to AUT MCS Principles of Operating Systems Course. This message is from a custom logger implemented by 40013021 and 40013027");
     log_warning("This is a test warning message for the custom logger");
     log_error("This is a test error message for the custom logger");
-    // <-- End of custom log messages -->
 
     kvminit();       // create kernel page table
     kvminithart();   // turn on paging
